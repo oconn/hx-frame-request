216bda5b24525014b38bc926e81c1739693afcf2
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2019-06-11 09:02:29 -0400

    create db ns

--- src/hx_frame_request/core.cljs
+++ src/hx_frame_request/core.cljs
@@ -1,23 +1,7 @@
 (ns hx-frame-request.core
   (:require
-   [cljs.spec.alpha :as s]
-
    [hx-frame-request.events :as hxr-events]
    [hx-frame-request.subscriptions :as hxr-subscriptions]))
-
-(def initial-state
-  "Initial hx-frame-request state.
-
-  All requests are associated into a map."
-  {:hx-frame-request {}})
-
-;; Specs for each individual request
-(s/def ::status #{:loading :success :failure})
-(s/def ::request-time number?)
-(s/def ::error (s/nilable map?))
-(s/def ::request (s/map-of keyword? (s/keys :req-un [::status
-                                                     ::request-time
-                                                     ::error])))
 
 (defn register-events
   "Registers hx-frame-request events and request handler"
---
+++ src/hx_frame_request/db.cljs
@@ -0,0 +1,16 @@
+(ns hx-frame-request.db
+  (:require [cljs.spec.alpha :as s]))
+
+(def initial-state
+  "Initial hx-frame-request state.
+
+  All requests are associated into a map."
+  {:hx-frame-request {}})
+
+;; Specs for each individual request
+(s/def ::status #{:loading :success :failure})
+(s/def ::request-time number?)
+(s/def ::error (s/nilable map?))
+(s/def ::request (s/map-of keyword? (s/keys :req-un [::status
+                                                     ::request-time
+                                                     ::error])))
